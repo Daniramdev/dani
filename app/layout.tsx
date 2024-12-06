fa6930d07f63bc0e29115a6cf5ac2a40f0a289d5
unknown <danird1240@gmail.com>
2024-12-06 21:41:09 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,37 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
-import StairTransition from "@/components/StairTransition"
-
-
+import StairTransition from "@/components/StairTransition";
+import Image from "next/image"; // Import Image component
 
 export const metadata: Metadata = {
   title: "Dani",
-  description: "Porto polio",
+  description: "Portfolio",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Portfolio" />
+        <meta name="author" content="Dani" />
+        <meta property="og:title" content="Dani's Portfolio" />
+        <meta property="og:description" content="Showcasing the work of Dani, a full-stack developer" />
+        <meta property="og:image" content="/path/to/your-image.jpg" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+        <link rel="icon" href="/images/logo.jpg" />
+      </head>
       <body>
         <Header />
+        
+     
         <StairTransition />
         <PageTransition>{children}</PageTransition>
       </body>
